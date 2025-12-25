9d1be50c0d22a7000bf473fceb6b998f1a3e2ad6
mark <marksheinberg01@gmail.com>
2025-12-25 12:13:13 +0200

    start

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef201e9ea30bc82f5acd3463f670f2592819cde1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938168e72ccb094f6753f8e8019736fceefaca5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
